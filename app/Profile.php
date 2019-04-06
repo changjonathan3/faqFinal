e929cf10c57e63729aae8550bcc290a50d91e04e
chang <jkc26@njit.edu>
2019-04-06 16:15:25 -0400

    Added user profile migrate, model, updated user seeder, created profiles

---
+++ app/Profile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    public function testSave()
+    {
+        $user = factory(\App\User::class)->make();
+        $user->save();
+        $profile = factory(\App\Profile::class)->make();
+        $profile->user()->associate($user);
+        $this->assertTrue($profile->save());
+    }
+}
