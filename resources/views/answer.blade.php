6809baaed7c61aad90461652047bbcf72bcdb6c3
chang <jkc26@njit.edu>
2019-05-05 15:24:45 -0400

    Completed upvote for answers

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -14,6 +14,12 @@
                         <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
                         </button>
                         {!! Form::close() !!}
+
+                        {{ Form::open(['method'  => 'patch', 'route' => ['answers.upvote', $question, $answer->id]])}}
+                        <button class="btn btn-warning float-right" value="submit" type="submit" id="submit">Vote Up!
+                        </button>
+                        {!! Form::close() !!}
+
                         <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
                             Edit Answer
                         </a>
